585ecc447ef25a6c1a592476c2a98f0b28fa53e7
César Almeida <cesaralmeida89@gmail.com>
2025-08-31 09:57:47 +0200

    Add menu bar

--- lab_manager/main.py
+++ lab_manager/main.py
@@ -1,4 +1,5 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QMessageBox
+from PySide6.QtGui import QAction
 
 from lab_manager.dashboard import Dashboard
 
@@ -10,3 +11,29 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self._dashboard_widget)
         self.setWindowTitle("Lab Manager")
         self.resize(960, 540)
+
+        self.create_actions()
+        self.create_menus()
+
+    def create_actions(self):
+        self._quit_act = QAction("&Cerrar", self, shortcut="Ctrl+Q", statusTip="Cerrar la aplicación", triggered=self.close)
+
+        self._about_act = QAction("&Acerca de Lab Manager", self, triggered=self.open_about)
+
+    def create_menus(self):
+        self._edit_menu = self.menuBar().addMenu("&Archivo")
+        self._edit_menu.addSeparator()
+        self._edit_menu.addAction(self._quit_act)
+
+        self._file_menu = self.menuBar().addMenu("&Herramientas")
+
+        self._help_menu = self.menuBar().addMenu("&Ayuda")
+        self._help_menu.addAction(self._about_act)
+
+    def open_about(self):
+        QMessageBox.about(self, "Acerca de Lab Manager",
+            """<b>Lab Manager</b><br>
+            Aplicación de gestión de laboratorio.<br><br>
+            Desarrollado por César Almeida<br>
+            Github: <a href="https://github.com/cesarforall">GitHub</a><br>
+            """)
